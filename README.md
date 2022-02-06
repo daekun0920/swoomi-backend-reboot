fe254956c86e0d87677096981967438e67033fbc
daekun0920 <dosemfb0920@gmail.com>
2022-02-06 21:20:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# swoomi-backend-reboot
---
+++ src/main/java/team/swoomi/backend/SwoomiBackendRebootApplication.java
@@ -0,0 +1,13 @@
+package team.swoomi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwoomiBackendRebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwoomiBackendRebootApplication.class, args);
+    }
+
+}
