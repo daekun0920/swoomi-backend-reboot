abbe10353dd3dc7d3031a1bbbbbd58d6c46cf793
daekun0920 <dosemfb0920@gmail.com>
2022-02-10 20:55:07 +0900

    CORS 설정

---
+++ src/main/java/team/swoomi/backend/controller/SummonerController.java
@@ -0,0 +1,12 @@
+package team.swoomi.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/summoner")
+@RequiredArgsConstructor
+public class SummonerController {
+
+}
